51f703b2717a0c0dbe3fe195e8931c2620e12a1f
Neil Forrester <neil@nforrester.net>
2022-10-15 16:25:13 -0700

    add more util tests

--- util.cpp
+++ util.cpp
@@ -31,6 +31,81 @@ bool twos_complement_test()
     T(-50, umax-49u);
     #undef T
 
+    if (sizeof(U) <= 2)
+    {
+        U u = 0;
+        do
+        {
+            S u2s = signed_from_unsigned_twos_complement<U, S>(u);
+            U u2s2u = unsigned_twos_complement_from_signed<U, S>(u2s);
+            test_assert(u == u2s2u);
+            ++u;
+        } while(u != 0);
+
+        S s = std::numeric_limits<S>::lowest();
+        while (true)
+        {
+            U s2u = unsigned_twos_complement_from_signed<U, S>(s);
+            S s2u2s = signed_from_unsigned_twos_complement<U, S>(s2u);
+            test_assert(s == s2u2s);
+            if (s == std::numeric_limits<S>::max())
+            {
+                break;
+            }
+            ++s;
+        }
+    }
+
+    return true;
+}
+
+bool mod_test()
+{
+    test_assert(mod(-9, 6) == 3);
+    test_assert(mod(-8, 6) == 4);
+    test_assert(mod(-7, 6) == 5);
+    test_assert(mod(-6, 6) == 0);
+    test_assert(mod(-5, 6) == 1);
+    test_assert(mod(-4, 6) == 2);
+    test_assert(mod(-3, 6) == 3);
+    test_assert(mod(-2, 6) == 4);
+    test_assert(mod(-1, 6) == 5);
+    test_assert(mod( 0, 6) == 0);
+    test_assert(mod( 1, 6) == 1);
+    test_assert(mod( 2, 6) == 2);
+    test_assert(mod( 3, 6) == 3);
+    test_assert(mod( 4, 6) == 4);
+    test_assert(mod( 5, 6) == 5);
+    test_assert(mod( 6, 6) == 0);
+    test_assert(mod( 7, 6) == 1);
+    test_assert(mod( 8, 6) == 2);
+    test_assert(mod( 9, 6) == 3);
+
+    return true;
+}
+
+bool nwraps_test()
+{
+    test_assert(nwraps(-9, 6) == -2);
+    test_assert(nwraps(-8, 6) == -2);
+    test_assert(nwraps(-7, 6) == -2);
+    test_assert(nwraps(-6, 6) == -1);
+    test_assert(nwraps(-5, 6) == -1);
+    test_assert(nwraps(-4, 6) == -1);
+    test_assert(nwraps(-3, 6) == -1);
+    test_assert(nwraps(-2, 6) == -1);
+    test_assert(nwraps(-1, 6) == -1);
+    test_assert(nwraps( 0, 6) ==  0);
+    test_assert(nwraps( 1, 6) ==  0);
+    test_assert(nwraps( 2, 6) ==  0);
+    test_assert(nwraps( 3, 6) ==  0);
+    test_assert(nwraps( 4, 6) ==  0);
+    test_assert(nwraps( 5, 6) ==  0);
+    test_assert(nwraps( 6, 6) ==  1);
+    test_assert(nwraps( 7, 6) ==  1);
+    test_assert(nwraps( 8, 6) ==  1);
+    test_assert(nwraps( 9, 6) ==  1);
+
     return true;
 }
 
@@ -42,6 +117,9 @@ bool util_test()
     T(int32_t, uint32_t);
     T(int64_t, uint64_t);
     #undef T
+
+    test_assert(mod_test());
+    test_assert(nwraps_test());
 
     return true;
 }
